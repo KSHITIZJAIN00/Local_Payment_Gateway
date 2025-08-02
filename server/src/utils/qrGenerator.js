1442d8f452cd6cf8d29a72dc2cce6af7cf75f424
KSHITIZJAIN00 <114683009+KSHITIZJAIN00@users.noreply.github.com>
2025-08-02 19:18:06 +0530

    Update qrGenerator.js

--- server/src/utils/qrGenerator.js
+++ server/src/utils/qrGenerator.js
@@ -1,8 +1,15 @@
-const QRCode = require("qrcode");
+const QRCode = require('qrcode');
 
-module.exports = {
-  generate: async (data) => {
-    // data will be a URL string
-    return QRCode.toDataURL(data);
+exports.generate = async function (payload) {
+  console.log("QR Generator called with payload:", payload);
+  try {
+    const dataString = JSON.stringify(payload);
+    console.log("Generating QR for:", dataString);
+    const qr = await QRCode.toDataURL(dataString);
+    console.log("QR generated successfully");
+    return qr;
+  } catch (err) {
+    console.error("Error generating QR:", err);
+    throw err;
   }
 };
